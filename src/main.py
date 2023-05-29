39f91c62a64fd79e70b21e4a683b43f3aca3d1ba
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-05-28 21:48:29 -0700

    project is ready

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
-
+from datetime import datetime
 from src.util import load_operations, output_date, description_of_transaction, sender_data, reciever_data, money_amount, \
     currency
 
 if __name__ == '__main__':
     filename = 'operations.json'
     operation = load_operations(filename)
-    final_operation = operation[:5]
+
+    sort_data = sorted(operation, key=lambda k: '.'.join(k['date'].split('.')))
+
+
+    final_operation = sort_data[-6:]
 
     for word in final_operation:
 
